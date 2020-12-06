e2555254ed78e23a1a32d7a5d39b05da5edbf936
Alejandro Rosas <alejandrorosasdev@gmail.com>
2020-12-06 16:03:24 +0000

    Fix flashing huge Font Awesome icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,7 @@ module.exports = {
         anonymize: true,
       },
     },
+    `gatsby-plugin-fontawesome-css`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
