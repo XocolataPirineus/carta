968e9eadb5e2d1034f4665fdf22c6b1b08596f98
Alejandro Rosas <alejandrorosasdev@gmail.com>
2021-03-07 16:49:34 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,8 @@ module.exports = {
         background_color: `#f4efea`,
         theme_color: `#201e1b`,
         display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`,
+        include_favicon: false,
       },
     },
     {
