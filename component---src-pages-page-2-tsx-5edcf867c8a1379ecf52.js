d2b8728d0662714097d47546b5b61d6fb7b057b9
alejandrorosas <alejandrorosas@users.noreply.github.com>
2026-02-11 08:26:41 +0000

    deployed via Gatsby Publish Action 🎩 for f80d782b69897fec79271d5a7ce5152881e8d0d4

---
+++ component---src-pages-page-2-tsx-5edcf867c8a1379ecf52.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcarta=self.webpackChunkcarta||[]).push([[179],{2539:function(e,t,a){a.r(t);var l=a(6540),c=a(4794),n=a(574),r=a(7528);t.default=e=>l.createElement(n.A,null,l.createElement(r.A,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2 (",e.path,")"),l.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}]);
+//# sourceMappingURL=component---src-pages-page-2-tsx-5edcf867c8a1379ecf52.js.map
